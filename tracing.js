0dfe72816ac97517b2686bf05e9e5d78e0412353
Ayush Arora <ayusharora@Admins-MacBook-Pro.local>
2022-07-08 15:35:44 +0530

    feat: tracing package init

---
+++ tracing.js
@@ -0,0 +1,58 @@
+"use strict";
+
+require("dotenv").config();
+const {
+  BasicTracerProvider,
+  BatchSpanProcessor,
+} = require("@opentelemetry/sdk-trace-base");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-grpc");
+const { Resource } = require("@opentelemetry/resources");
+const {
+  SemanticResourceAttributes,
+} = require("@opentelemetry/semantic-conventions");
+
+const opentelemetry = require("@opentelemetry/sdk-node");
+const {
+  getNodeAutoInstrumentations,
+} = require("@opentelemetry/auto-instrumentations-node");
+
+const SERVICE_NAME = process.env.SERVICE_NAME;
+
+if (!SERVICE_NAME) {
+  throw new Error(
+    "No service name specified in environment, Please add SERVICE_NAME in the .env"
+  );
+}
+
+const exporter = new OTLPTraceExporter();
+
+const provider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: SERVICE_NAME,
+  }),
+});
+
+provider.addSpanProcessor(new BatchSpanProcessor(exporter));
+
+provider.register();
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: exporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk
+  .start()
+  .then(() => {
+    console.log("Tracing initialized");
+  })
+  .catch((error) => console.log("Error initializing tracing", error));
+
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => console.log("Tracing terminated"))
+    .catch((error) => console.log("Error terminating tracing", error))
+    .finally(() => process.exit(0));
+});
